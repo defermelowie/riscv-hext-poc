ccc93838c3c4478299dda208c2c7a37effb656e0
Lowie Deferme <lowie.deferme@student.kuleuven.be>
2023-05-04 21:59:40 +0200

    feat: add syscalls for hypervisors

---
+++ lib/syscall_encoding.h
@@ -0,0 +1,4 @@
+#define ECALL_EXIT_SUCCESS  0x0 // Exit with success
+#define ECALL_EXIT_FAILURE  0x1 // Exit with failure
+#define ECALL_CSR_MISA      0x2 // Get ISA register
+#define ECALL_CSR_MHARTID   0x3 // Get hardware thread
