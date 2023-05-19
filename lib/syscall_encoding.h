b7b1a42d8c2051669acc67c6247b75b36884fd10
Lowie Deferme <lowie.deferme@student.kuleuven.be>
2023-05-19 21:10:16 +0200

    feat: add meaning of life ecall

--- lib/syscall_encoding.h
+++ lib/syscall_encoding.h
@@ -1,4 +1,5 @@
-#define ECALL_EXIT_SUCCESS  0x0 // Exit with success
-#define ECALL_EXIT_FAILURE  0x1 // Exit with failure
-#define ECALL_CSR_MISA      0x2 // Get ISA register
-#define ECALL_CSR_MHARTID   0x3 // Get hardware thread
+#define ECALL_EXIT_SUCCESS    0x0 // Exit with success
+#define ECALL_EXIT_FAILURE    0x1 // Exit with failure
+#define ECALL_CSR_MISA        0x2 // Get ISA register
+#define ECALL_CSR_MHARTID     0x3 // Get hardware thread
+#define ECALL_MEANING_OF_LIFE 0x8 // Get the meaning of life
